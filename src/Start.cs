a40cf6729fa401e2c5f7b6a9ac6bfce5511addb2
Nico Lerchl <nico@lerchl.one>
2022-09-14 16:05:03 +0200

    api basics

--- src/Start.cs
+++ src/Start.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
@@ -20,8 +21,25 @@ class Start {
         string text = Encoding.ASCII.GetString(buffer, 0, lenght);
         Console.WriteLine(text);
 
-        client.Disconnect(false);
 
+        string pattern = @"^(?'httpMethod'\w+) (?'endpoint'/\w*)";
+        Regex regex = new Regex(pattern);
+        Match match = regex.Match(text);
+        string httpMethod = match.Groups["httpMethod"].Value;
+        string apiEndpoint = match.Groups["endpoint"].Value;
+
+        if (apiEndpoint.Equals("/users")) {
+            Console.WriteLine($"{httpMethod}-Request an /users");
+            client.Send(Encoding.ASCII.GetBytes("200 USER CREATED"));
+        } else if (apiEndpoint.Equals("/cards")) {
+            Console.WriteLine($"{httpMethod}-Request an /cards");
+            client.Send(Encoding.ASCII.GetBytes("200 CARD CREATED"));
+        } else {
+            Console.WriteLine("Anfrage an Endpoint der nicht existiert.");
+            client.Send(Encoding.ASCII.GetBytes("400 ENDPOINT DOES NOT EXIST"));
+        }
+
+        client.Disconnect(false);
         Console.WriteLine("Connection closed...");
     }
 }
---
+++ src/api/Users.cs
@@ -0,0 +1,7 @@
+namespace Api {
+
+    internal class Users {
+
+        
+    }
+}
