e20b258cd3a231cc191ef1c82c4f71bde0161381
Nico Lerchl <nico@lerchl.one>
2023-01-15 20:26:59 +0100

    fix: synchronize battle queue

--- MonsterTradingCardGame/src/logic/battle/BattleHandler.cs
+++ MonsterTradingCardGame/src/logic/battle/BattleHandler.cs
@@ -2,6 +2,9 @@ using MonsterTradingCardGame.Data.BattleNS;
 
 namespace MonsterTradingCardGame.Logic.BattleNS {
 
+    /// <summary>
+    ///     Singleton for handling battles.
+    /// </summary>
     internal class BattleHandler {
 
         private static BattleHandler? _instance;
@@ -19,6 +22,7 @@ namespace MonsterTradingCardGame.Logic.BattleNS {
 
         private readonly BattleLogic _battleLogic = new();
         private readonly List<BattleQueueEntry> _queue = new();
+        private readonly Mutex _queueMutex = new();
 
         // /////////////////////////////////////////////////////////////////////
         // Init
@@ -32,16 +36,21 @@ namespace MonsterTradingCardGame.Logic.BattleNS {
         // Methods
         // /////////////////////////////////////////////////////////////////////
 
-        // TODO: Method should only be called by one thread at a time
         /// <summary>
         ///     Queues a User to battle
         /// </summary>
+        /// <param name="userId">The User's Id</param>
+        /// <returns>The BattleQueueEntry whose <see cref="ManualResetEventSlim"/>
+        ////    should be waited for as it will be set when the battle is finished.
+        //// </returns>
         public BattleQueueEntry QueueBattle(Guid userId) {
+            _queueMutex.WaitOne();
             BattleQueueEntry? entry = FindBattleForUser(userId);
 
             if (entry == null) {
                 BattleQueueEntry newEntry = new(new(), new(userId));
                 _queue.Add(newEntry);
+                _queueMutex.ReleaseMutex();
                 return newEntry;
             }
 
@@ -54,6 +63,7 @@ namespace MonsterTradingCardGame.Logic.BattleNS {
             });
 
             _queue.Remove(entry);
+            _queueMutex.ReleaseMutex();
             return entry;
         }
 
